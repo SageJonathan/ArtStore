aed57a224d154125d4ff46585f32fd4ce1411018
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-12-27 16:49:02 -0500

    correct img styles to allow overlay

--- src/components/gallery/gallery-grid.tsx
+++ src/components/gallery/gallery-grid.tsx
@@ -1,11 +1,13 @@
-
-import Image from "next/image";
-import background from "../../../public/grid.jpg";
-
-export default function GalleryGrid () {
-    return (
-        <div className="w-full h-[calc(100vh-68px)]">
-        <Image src={background} alt="gallery background" className="w-full h-full object-cover" />
+export default function GalleryGrid() {
+  return (
+    <div
+      className="relative w-full h-[calc(100vh-68px)] bg-cover bg-center"
+      style={{ backgroundImage: `url('/grid.jpg')` }}
+    >
+      {/* Overlay content */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-white text-3xl font-bold">Welcome to the Grid</h1>
+      </div>
     </div>
-    );
+  );
 }
--- src/components/gallery/gallery-live.tsx
+++ src/components/gallery/gallery-live.tsx
@@ -1,12 +1,16 @@
-
-import Image from "next/image";
-import background from "../../../public/gallery.png";
-
-export default function GalleryLive () {
+export default function GalleryLive() {
+  return (
     // mobile = 1 picture/ tablet=2/desktop=3
-    return (
-        <div className="w-full h-[calc(100vh-68px)]">
-            <Image src={background} alt="gallery background" className="w-full h-full object-cover" />
-        </div>
-    );
+    <div
+      className="relative w-full h-[calc(100vh-68px)] bg-cover bg-center"
+      style={{ backgroundImage: `url('/gallery.png')` }}
+    >
+      {/* Overlay content */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-white text-3xl font-bold">
+          Welcome to the Gallery
+        </h1>
+      </div>
+    </div>
+  );
 }
