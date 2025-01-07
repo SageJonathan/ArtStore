87bd08aaf89041d548fd1a6c87f559c66f3b00eb
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-01-07 16:27:26 -0500

    update gird lgoci to pull form server

--- src/components/gallery/gallery-grid.tsx
+++ src/components/gallery/gallery-grid.tsx
@@ -1,114 +1,29 @@
 //TODO: Update ternary to use db logic property
 // Update props to server servce and db
 
-"use client";
 
-import { useEffect, useState } from "react";
+import { displayArt} from '@/app/db/queries/art';
 import Image from 'next/image';
 import Tag from "@/components/gallery/gallery-tag";
 
 
 interface Painting {
-  src: string;
-  title: string;
-  dimensions: string;
-  price: string;
+  imageUrlFront: string
+   title: string 
+   size: string
+  isVertical: boolean
+  inStock: boolean
 }
 
-export default function GalleryGrid() {
- const [paintings, setPaintings] = useState<Painting[]>([]);
+export default async function GalleryGrid({imageUrlFront,title,size,isVertical,inStock}:Painting) {
 
-  // Dummy data -- later needs to come from DB
-  useEffect(() => {
-    const paintingData = [
-      {
-        src: "/art2.png",
-        title: "Mountain Retreat",
-        dimensions: "50 x 70cm",
-        price: "$700",
-      },
-      {
-        src: "/art3.png",
-        title: "Sunny Beach",
-        dimensions: "40 x 50cm",
-        price: "$400",
-      },
-      {
-        src: "/art4.png",
-        title: "Forest View",
-        dimensions: "60 x 80cm",
-        price: "$900",
-      },
-      {
-        src: "/art5.png",
-        title: "Cityscape",
-        dimensions: "30 x 30cm",
-        price: "$300",
-      },
-      {
-        src: "/art6.png",
-        title: "Night Sky",
-        dimensions: "80 x 100cm",
-        price: "$1200",
-      },
-      {
-        src: "/art7.png",
-        title: "Abstract Colors",
-        dimensions: "40 x 40cm",
-        price: "$350",
-      },
-      {
-        src: "/art8.png",
-        title: "Golden Horizon",
-        dimensions: "60 x 90cm",
-        price: "$850",
-      },
-      {
-        src: "/art9.png",
-        title: "Ocean Breeze",
-        dimensions: "50 x 60cm",
-        price: "$750",
-      },
-      {
-        src: "/art10.png",
-        title: "Silent River",
-        dimensions: "45 x 55cm",
-        price: "$600",
-      },
-      {
-        src: "/art11.png",
-        title: "Waves of Time",
-        dimensions: "70 x 80cm",
-        price: "$950",
-      },
-      {
-        src: "/art12.png",
-        title: "Autumn Path",
-        dimensions: "40 x 60cm",
-        price: "$650",
-      },
-      {
-        src: "/art13.png",
-        title: "Mountain Peaks",
-        dimensions: "30 x 40cm",
-        price: "$500",
-      },
-      {
-        src: "/art14.png",
-        title: "The Quiet Lake",
-        dimensions: "50 x 70cm",
-        price: "$700",
-      },
-      {
-        src: "/art1.png",
-        title: "House With River",
-        dimensions: "30 x 40cm",
-        price: "$500",
-      },
-    ];
-    setPaintings(paintingData);
-  }, []);
-
+  const paintings= await displayArt ();
+  
+  if (!paintings){
+    return
+    <div>Loading galery...</div>
+  }
+ else 
   return (
 <div
   className="w-full min-h-screen bg-center pt-10 bg-gray-100"
@@ -126,14 +41,12 @@ export default function GalleryGrid() {
           className="w-full h-full overflow-hidden flex flex-col justify-center items-center"
         >
           <Image
-            src={painting.src}
+            src={painting.imageUrlFront}
             alt={`Painting ${index + 1}`}
-            width={painting.src === "/art1.png" || painting.src === "/art14.png" ? 240 : 500}
+            width={painting.isVertical ? 240 : 500}
             height={320} 
             className={`object-contain ${
-              painting.src === "/art1.png" || painting.src === "/art14.png"
-                ? "sm:w-60 md:w-40"
-                : "w-full"
+              painting.isVertical ? "sm:w-60 md:w-40": "w-full"
             }`}
           />
              <div className="mt-5 text-left pb-20 md:pb-40">
@@ -142,7 +55,7 @@ export default function GalleryGrid() {
                   {painting.title}
                 </h3>
                 <p className="text-xs font-medium text-gray-800 pl-1 font-merriweather">
-                  {painting.dimensions}
+                  {painting.size}
                 </p>
               </div>
             </div>
