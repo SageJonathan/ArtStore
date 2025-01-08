3e79a05c77f45cf621445703c24b7b0c0e4e97d9
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-01-08 11:29:46 -0500

    ready for logic implemntation

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -16,56 +16,60 @@ export default function CartPage() {
     //Checkout-container
     <div className="flex flex-col m-10">
       <div className="md:flex md:flex-row">
-
         {/* Left Side: Two smaller images in a column */}
         <div className="relative flex flex-row w-full md:w-1/2 lg:w-3/4 justify-center">
           <div className="absolute top-0 left-0">
             <div className="">
-              <Image src="/art1.png" alt="front side" 
-              width={45} height={45}/>
+              <Image src="/art1.png" alt="front side" width={45} height={45} />
             </div>
             <div className="pt-5">
-              <Image src="/artback1.png" alt="back side" 
-               width={45} height={45}/>
+              <Image
+                src="/artback1.png"
+                alt="back side"
+                width={45}
+                height={45}
+              />
             </div>
           </div>
           {/* Middle: Large image */}
           {/* Result of toggle- front is default */}
           <div className="ml-12 lg:ml-0 ">
-            <Image src="/art1.png" alt="Main Img" 
-             width={350} height={350}/>
+            <Image src="/art1.png" alt="Main Img" width={350} height={350} />
           </div>
         </div>
 
         {/* Right Side: Image details, tax, shipping, and payment */}
         {/* Dynamic data. need to drill in paintings && 2 seprate api calls && add total */}
-        <div className="flex flex-col border w-full md:w-1/2 lg:w-1/4 p-5">
+        <div className="flex flex-col justify-center border pl-2 w-full md:w-1/2 lg:w-1/4">
           <div className="">
-            <div className="">
-              <h1>Title: {}</h1>
-              <p>Size: {}</p>
-              <p>Price: {}CAD</p>
+            <div className="leading-relaxed">
+              <h1 className="font-bold">Art Piece:</h1>
+              <p>Title: lorem lorem lorem{}</p>
+              <p>Size: 45 x 44"{}</p>
+              <p>Price: 400{}.00 CAD</p>
             </div>
             {/* Use this data for api call for above */}
-            <div className="">
+            <div className="my-5">
               <form action="submit">
-                <label htmlFor="">Enter Shipping Adress:</label>
+                <label htmlFor="" className="font-bold">
+                  Enter Shipping Adress:
+                </label>
                 <input
                   id="shipping-address"
                   type="text"
                   name="shippingAddress"
-                //   Place holder must match API requirments
+                  //   Place holder must match API requirments
                   placeholder="123 Main St, City, Country"
                   className="p-2 border rounded-md flex flex-col"
                   required
                 ></input>
               </form>
             </div>
-            <div className="">
-              <h1>Estimated Cost:</h1>
-              <p>Tax: {}CAD</p>
-              <p>Shipping Cost: {}CAD</p>
-              <p>Total Cost: {}CAD</p>
+            <div className="mb-5 leading-relaxed">
+              <h1 className="font-bold">Estimated Cost:</h1>
+              <p>Tax: {}10.00 CAD</p>
+              <p>Shipping Cost:{} 20.00 CAD</p>
+              <p>Total Cost: {}430. 00CAD</p>
             </div>
           </div>
           {/* We need to get some info from payment processing to get cleint info */}
@@ -103,13 +107,27 @@ export default function CartPage() {
         </div>
       </div>
       {/* Description below */}
-      <div className="">
-        <p>
-          This is the description of the product, providing details and any
-          other relevant information about the item being purchased. **
-          Certificate of authenticity ** All sies include frames ** All shipping
-          is through FedEx ** Canada/USA shipping
-        </p>
+      <div className="mt-10 p-4 bg-gray-100 border shadow-md flex flex-col md:flex-row justify-between">
+        <div>
+        <p>{} medium</p>
+            <p>{}description</p>
+        </div>
+        <div>
+  <p>
+    <strong>Comes with a Certificate of Authenticity</strong>
+  </p>
+  <p>
+    <strong>The dimensions listed are inclusive of the frame</strong>
+  </p>
+  <br />
+  <p>
+    <strong>Shipping is handled exclusively through FedEx</strong>
+  </p>
+  <p>
+    <strong>Available for shipping within Canada and the USA</strong>
+  </p>
+</div>
+
       </div>
     </div>
   );
