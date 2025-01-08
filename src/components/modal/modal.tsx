37635491bd7ecb21aba4f517abb9d00048b78981
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-01-08 13:55:46 -0500

    add link to cart

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,45 +1,47 @@
-'use client'
+"use client";
 
-import Modal from 'react-modal';
-import './modal.css'; 
-
+import Link from "next/link";
+import Modal from "react-modal";
+import "./modal.css";
 
 interface Painting {
-    imageUrlFront: string
-     title: string 
-     size: string
-    isVertical: boolean
-    inStock: boolean
-  }
-
+  imageUrlFront: string;
+  title: string;
+  size: string;
+  isVertical: boolean;
+  inStock: boolean;
+}
 interface ModalProps {
-    isOpen: boolean;
-    onRequestClose: () => void;
-    painting: Painting;
-  }
+  isOpen: boolean;
+  onRequestClose: () => void;
+  painting: Painting;
+}
 
-  Modal.setAppElement('#__next');
+Modal.setAppElement("#__next");
 
-
-export default function ArtDetails({ isOpen, onRequestClose, painting }: ModalProps) {
-    return (
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        contentLabel="Inventory Item Delete Confirmation"
-        className="modal-container"
-        overlayClassName="modal-overlay"
-      >
-         
-        <div>
-          <button className="absolute left-5 bottom-1" onClick={onRequestClose}>Return</button>
-          <button className="absolute right-5 bottom-1" onClick={() => {
+export default function ArtDetails({isOpen,onRequestClose,painting,}: ModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Inventory Item Delete Confirmation"
+      className="modal-container"
+      overlayClassName="modal-overlay"
+    >
+      <div>
+        <button className="absolute left-5 bottom-1" onClick={onRequestClose}>
+          Return
+        </button>
+        <button
+          className="absolute right-5 bottom-1"
+          onClick={() => {
             confirm();
             onRequestClose();
-          }}>Buy Now</button>
-        </div>
-      </Modal>
-    );
-  }
-
-  
+          }}
+        >
+          <Link href="/cart">Buy Now</Link>
+        </button>
+      </div>
+    </Modal>
+  );
+}
