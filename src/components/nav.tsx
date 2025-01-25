4c9b80f73448b9e2a82d43a1702a489ed1778eeb
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-01-25 16:31:24 -0700

    remove cart from nav/ make mobile style unique

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,32 +5,25 @@ export default function Nav() {
     <div className="flex flex-row justify-between items-end bg-slate p-4">
       <div>
         <Link href="/">
-        <h1 className="text-black text-3xl font-bold ml-3 font-playfair">
-          Louise Guay
-        </h1>
+          <h1 className="text-black text-3xl font-bold ml-3 font-playfair flex flex-col sm:flex-row">
+            Louise
+            <span className="sm:ml-2">Guay</span>
+          </h1>
         </Link>
       </div>
 
-      {/* Navigation Section */}
-      <div className="flex flex-row space-x-8 mr-3 items-end">
-        <div>
-          <Link href="/about">
-            <h3 className="text-black text-2xl font-playfair cursor-pointer hover:underline">
-              About
-            </h3>
-          </Link>
-        </div>
+      <div className="flex flex-col sm:flex-row sm:space-x-8  sm:mr-3 sm:items-end">
         <div>
           <Link href="/">
             <h3 className="text-black text-2xl font-playfair cursor-pointer hover:underline">
-              Works
+              <span className="sm:block md:hidden">|</span> Works
             </h3>
           </Link>
         </div>
         <div>
-          <Link href="/cart">
+          <Link href="/about">
             <h3 className="text-black text-2xl font-playfair cursor-pointer hover:underline">
-              Cart
+              <span className="sm:block md:hidden">|</span> About
             </h3>
           </Link>
         </div>
