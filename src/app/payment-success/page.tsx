77cbf6c2fc85a7649c3a69b7c4bc59964ac204f5
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-01-27 17:26:13 -0700

    style succes payment page all sizes

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -7,16 +7,16 @@ export default function PaymentSuccess () {
   const amount= searchParams.get("amount");
   
     return (
-      <main className=" w-full h-full p-10 text-white text-center border  rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <main className=" w-full h-full text-white text-center border bg-gradient-to-tr from-blue-500 to-purple-500">
         <div className="mb-10">
-          <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-          <h2 className="text-2xl">You successfully sent</h2>
+          <h1 className="text-4xl font-extrabold mb-2 pt-10">Thank you!</h1>
+          <h2 className="text-2xl mb-5">You successfully sent</h2>
   
-          <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
+          <div className="bg-white p-2 mx-20 rounded-md text-purple-500 text-4xl font-bold">
             ${amount}
           </div>
         </div>
-        <div className="bg-cover bg-center" style={{ backgroundImage: `url('/flowers.webp')`, height: "60vh" }}></div>
+        <div className="bg-cover bg-center" style={{ backgroundImage: `url('/flowers.webp')`, height: "65vh" }}></div>
       </main>
     );
   }
